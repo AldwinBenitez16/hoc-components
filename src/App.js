5c0e00badd0769cd0ce6be61090658f9db4e7132
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-10-19 22:09:50 -0400

    Updated App.js as a class that uses HOC.js
    - App that js is now a class that uses the higher order component HOC.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Hoc from './HOC';
 
-function App() {
-  return (
-    <div >
-
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        Higher-Order Components
+      </div>
+    )
+  }
 }
 
+App = Hoc(App);
 export default App;
